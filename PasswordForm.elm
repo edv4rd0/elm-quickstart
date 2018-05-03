e24b47bd21c2381a19a87d2b6a3b64fe59fa030b
Edward Williams <edward.pendragon@gmail.com>
2018-05-03 16:42:40 +1200

    Little bit of playing around with string validation

--- PasswordForm.elm
+++ PasswordForm.elm
@@ -1,8 +1,9 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import Char exposing (isDigit, isLower, isUpper)
 
-
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
@@ -58,10 +59,20 @@ viewValidation : Model -> Html msg
 viewValidation model =
   let
     (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
+      if model.password /= model.passwordAgain then
         ("red", "Passwords do not match!")
+      else if String.length model.password == 0 then
+        ("green", "Please enter a password")
+      else if String.length model.password < 8 then
+        ("red", "Password must be at least eight characters")
+      else if not (String.any isDigit model.password) then
+        ("red", "Password must contain a number!") 
+      else if not (String.any isLower model.password) then
+        ("red", "Password must contain lowercase characters!")
+      else if not (String.any isUpper model.password) then
+        ("red", "Password must contain uppercase characters!")
+      else
+        ("green", "OK")
   in
     div [ style [("color", color)] ] [ text message ]
       
